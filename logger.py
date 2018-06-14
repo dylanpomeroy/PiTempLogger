4cd5f621bcf23f22c1d0985d4eccf5d6996c92f4
Dylan Pomeroy <dylan.pomeroy@redquantum.ca>
2018-06-13 21:45:17 -0300

    adding logging and time

--- logger.py
+++ logger.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import time
+import logging
+import datetime
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
@@ -22,9 +24,8 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        return temp_c
 
 while True:
-    print(read_temp())
-    time.sleep(1)
+    logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + read_temp())
+    time.sleep(1) # reading takes about 1 sec
